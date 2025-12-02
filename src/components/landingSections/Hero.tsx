ef3449866b0861ec2109d4b4151ef583b343280f
Mheyrie <omota4fidelity@gmail.com>
2025-12-02 14:29:46 +0000

    feat: implement Hero section with navigation and layout

--- src/components/landingSections/Hero.tsx
+++ src/components/landingSections/Hero.tsx
@@ -1,5 +1,15 @@
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section className="relative bg-white">
+      <nav className="bg-linear-to-r from-b-[#A4A3A5] to-[#D6D2D6] rounded-3xl ">
+        <div className="">
+          Logo Here
+        </div>
+        <div className="bg-gradient-to-br from-orange-400 via-yellow-500 to-green-900">
+          Navigation Links Here
+        </div>
+      </nav>
+      <div>Hero Section</div>
+    </section>
+  );
 }
