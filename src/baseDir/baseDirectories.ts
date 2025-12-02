1667b15677a48c96a089947bb4553c5b273a1672
Mheyrie <omota4fidelity@gmail.com>
2025-12-02 10:57:35 +0000

    feat: add baseDirectories module for managing image paths

---
+++ src/baseDir/baseDirectories.ts
@@ -0,0 +1,11 @@
+const BaseDirectories = {
+  /** Directories and folders. */
+  IMAGES_DIR: "/images",
+  LOGOS_DIR: "/images/logos",
+  ICONS_DIR: "/images/icons",
+
+  // Usage
+  //    <img src={`${BaseDirectories.IMAGES_DIR}/403.png`}  alt="Trade Lenda"/>
+};
+
+export default BaseDirectories;
