d8f1f984a085d2e55323ab6bd714726741c05f2d
Mheyrie <omota4fidelity@gmail.com>
2025-12-02 11:23:35 +0000

    feat: add Dropdown component with basic structure

--- src/pages/landing/Home.tsx
+++ src/pages/landing/Home.tsx
@@ -1,14 +1,12 @@
-import { Helmet } from "react-helmet-async";
+
 import Hero from "../../components/landingSections/Hero";
 
 export default function Home() {
   return (
-    <div>
-        <Helmet>    
-            <title>Home - MyApp</title>
-            <meta name="description" content="Welcome to the Home page of Load Stone." />
-        </Helmet>
+    <>
+        <title> Home - Loadstone</title>
+        <meta name="description" content="Welcome to Loadstone" />  
         <Hero />
-    </div>
+    </>
   )
 }
