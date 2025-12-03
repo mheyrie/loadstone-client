c9e4feeee42a62ee6a023cdd0b7db17e8d190a3d
Mheyrie <omota4fidelity@gmail.com>
2025-12-03 11:28:00 +0000

    feat: add Products page and update routing to include it; adjust Navbar styles

--- src/components/_shared/Navbar.tsx
+++ src/components/_shared/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
 
   return (
     <section
-      className={`px-4 py-2 fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
+      className={`px-4 fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
         isScrolled ? "bg-black/20 backdrop-blur-md shadow-md" : "bg-transparent"
       }`}
     >
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -3,12 +3,14 @@ import Home from "../pages/landing/Home";
 import AboutUs from "../pages/landing/AboutUs";
 import Business from "../pages/landing/Business";
 import Support from "../pages/landing/Support";
+import Products from "../pages/landing/Products";
 
 export default function MainRoutes() {
   return (
     <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about-us" element={<AboutUs/>} />
+        <Route path="/products" element={<Products/>} />
         <Route path="/business" element={<Business/>} />
         <Route path="/support" element={<Support/>} />
     </Routes>
