037335cd6c6629df06c9f8578d28467a5b5b6c1d
Mheyrie <omota4fidelity@gmail.com>
2025-12-03 07:00:14 +0000

    feat: implement navigation links in Hero section

--- src/components/landingSections/Hero.tsx
+++ src/components/landingSections/Hero.tsx
@@ -1,3 +1,5 @@
+import { navLink } from "../../data/landing";
+import { NavLink } from "react-router-dom";
 export default function Hero() {
 
   
@@ -6,7 +8,15 @@ export default function Hero() {
       <nav className="bg-linear-to-r from-b-[#A4A3A5] to-[#D6D2D6] rounded-3xl ">
         <div className="">Logo Here</div>
         <div className="bg-linear-to-br from-orange-400 via-yellow-500 to-green-900">
-          Navigation Links Here
+          {navLink.map((link) => (
+            <NavLink
+              key={link.text}
+              to={link.url}
+              className="px-4 py-2 text-white hover:underline"
+            >
+              {link.text}
+            </NavLink>
+          ))} 
         </div>
       </nav>
       <div className="grid grid-cols-2 bg-linear-to-br from-blue-400 via-purple-500 to-pink-600">
