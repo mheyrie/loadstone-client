d763e151ff28f6420cae8dccc066dd3e1307dcc6
Mheyrie <omota4fidelity@gmail.com>
2025-12-29 00:24:18 +0000

    Refactor BVN validation modal with updated labels and input fields

--- src/components/ui/modal/onboarding/Bvn.tsx
+++ src/components/ui/modal/onboarding/Bvn.tsx
@@ -1,5 +1,7 @@
 import BaseDirectories from "@/baseDir/baseDirectories";
 import Button from "../../Button";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "../../input";
 
 export default function Bvn() {
   return (
@@ -7,19 +9,26 @@ export default function Bvn() {
       {" "}
       <div className="flex flex-col items-center mb-6 md:mt-12">
         <img src={`${BaseDirectories.ICONS_DIR}/call.png`} alt="Auth Key" />
-        <small className="text-gray-500 text-md">Email verification Successful</small>
+        <p className="text-gray-500 text-md">Phone verification Successful</p>
       </div>{" "}
       <h3 className="text-2xl text-gray-100 font-extrabold">
-        Enter Your Phone Number
+      BVN VALIDATION
       </h3>
       <p className="text-gray-500 mt-6 flex items-start!">
-        Mobile number
+      Industry regulation requires us to collect this information to verify your identity.
       </p>
-      
+      <div className="">
+        <Label className="text-gray-500 mt-6 mb-2 flex items-start!">BVN (Bank Verification Number) <span className="text-red-500">*</span></Label>
+        <Input type="text" placeholder="Enter your bank verification number" className="w-80 md:w-96" />
+      </div>
+      <div className="">
+        <Label className="text-gray-500 mt-6 mb-2 flex items-start!">NIN (National Identity Number *<span className="text-red-500">*</span></Label>
+        <Input type="text" placeholder="National Identity number" className="w-80 md:w-96" />
+      </div>
       <Button
         content="Validate BVN"
         classes="primary-btn btn-md my-6 !w-full"
-        type="submit"
+        type="button"
       />
     </div>
   )
