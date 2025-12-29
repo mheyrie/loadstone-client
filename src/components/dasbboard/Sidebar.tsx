e21a18f2dc7b12d5f1ef45144f7473917b657f4e
Mheyrie <omota4fidelity@gmail.com>
2025-12-29 22:22:40 +0000

    Refactor Sidebar component layout and improve styling for better user experience

--- src/components/dasbboard/Sidebar.tsx
+++ src/components/dasbboard/Sidebar.tsx
@@ -1,35 +1,36 @@
 import { sideBar } from "@/data/landing";
-import { mdiContentCopy } from "@mdi/js";
+import { mdiAccountVoiceOff, mdiContentCopy } from "@mdi/js";
 import Icon from "@mdi/react";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col h-full justify-around ">
-      <div className="">
+    <div className="flex flex-col h-full mx-4 justify-between ">
+      <div className="mt-24">
         {" "}
-        <div className="">
-          <span className="">Personal referral code:</span>
-          <span className="bg-[#F3E8FF] flex justify-between items-center">
-            <span className="text-brand-purple">CYMKRGB</span>
+        <div className="mb-4">
+          <small className="block text-start">Personal referral code:</small>
+          <span className="bg-[#F3E8FF] text-brand-purple h-10 px-4 flex justify-between rounded-md items-center">
+            <span className=" font-bold">CYMKRGB</span>
             <Icon path={mdiContentCopy} className="size-4" />
           </span>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col space-y-6">
           {sideBar.map((link) => {
             return (
               <div className="relative group " key={link.text}>
                 <NavLink
                   to={link.url}
+                  end={link.url === "/dashboard"} 
                   className={({ isActive }) =>
-                   `flex px-4 ${isActive
-                      ? "text-brand-white flex px-4 bg-brand-purple"
+                   ` font-bold flex items-center px-4 space-x-2 py-2 ${isActive
+                      ? "text-brand-white flex bg-brand-purple rounded-md "
                       : " flex text-start text-gray-800"
                  }`  }
                 >
                   {" "}
-                  <Icon path={link.icon} className="size-4" />
-                  <span>{link.text}</span>
+                  <Icon path={link.icon} className="size-4 font-bold" />
+                  <span className="">{link.text}</span>
                 </NavLink>
               </div>
             );
@@ -37,8 +38,9 @@ const Sidebar = () => {
         </div>
       </div>
 
-      <div className="">
-        <h3 className="">Sign out</h3>
+      <div className="inline-flex items-center justify-start px-4 py-2 gap-2 mb-6 cursor-pointer hover:bg-red-300 rounded-md text-bold">
+        <Icon path={mdiAccountVoiceOff} className="size-4 text-gray-800 " />
+        <span className="">Sign Out</span>
       </div>
     </div>
   );
