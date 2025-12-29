fe19f32ea89a4c0f052b068fc9bfeb07ae331d67
Mheyrie <omota4fidelity@gmail.com>
2025-12-29 00:55:28 +0000

    Refactor code structure for improved readability and maintainability

--- src/pages/landing/Finance.tsx
+++ src/pages/landing/Finance.tsx
@@ -10,7 +10,7 @@ export default function Finance() {
         name="description"
         content="Explore financial solutions with Loadstone Financial"
       />
-      <section className="min-h-screen">
+      <section className="">
         <div
           className=""
           style={{
@@ -21,7 +21,7 @@ export default function Finance() {
         >
           {" "}
           <div
-            className="px-2 flex flex-col py-20 text-center justify-between h-full gap-2  md:mx-20"
+            className="px-2 flex pt-10 text-center justify-between gap-2  md:mx-20"
             data-aos="fade-right"
             data-aos-offset="70"
             data-aos-delay="0"
@@ -29,6 +29,11 @@ export default function Finance() {
             data-aos-easing="ease-in-out"
             data-aos-mirror="true"
             data-aos-once="true"
+            style={{
+              backgroundImage: "url('/images/landing/herobg2.png')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
           >
             <div className="text-white text-start flex flex-col mt-20 gap-6 ">
               <h3 className="uppercase text-start text-brand-purple-light">
@@ -53,39 +58,86 @@ export default function Finance() {
                 className="max-w-full md:mt-16"
               />
             </div>
-          </div> </div>
-          <section
-            className="grid min-h-screen  grid-rows-1 px-2  items-center py-20 text-center justify-between h-full gap-2  md:mx-20"
-            data-aos="fade-up"
-            data-aos-offset="70"
-            data-aos-delay="3"
-            data-aos-mirror="true"
+          </div>{" "}
+        </div>
+        <section
+          className="grid  grid-rows-1 px-2  items-center gap-2  md:mx-16"
+          data-aos="fade-up"
+          data-aos-offset="70"
+          data-aos-delay="3"
+          data-aos-mirror="true"
+        >
+          <div
+            className="grid md:grid-cols-2 grid-cols-1 justify-center  text-start items-center gap-8"
+            data-aos="fade-down"
           >
-            <div className="" data-aos="fade-down">
-              <div className="">
-                <h2 className="">
-                  Streamline your supply chain with our local purchase order
-                  financing,
-                </h2>
-                <p className="">
-                  Are you a small or medium-sized business looking to expand
-                  your operations and take on large orders? with our purchase
-                  order financing you can do the following:
-                </p>
-                <Button content="Get Funding Now" classes="secondary-btn btn-sm " />
-              </div>
-              <div className="flex items-center justify-center text-white">
-                <img
-                  src={`${BaseDirectories.IMAGES_DIR}/landing/Finance/img5.png`}
-                  alt="Hero"
-                  className="max-w-full md:mt-16"
-                />
-              </div>
+            <div className="">
+              <h1 className="font-bold text-4xl mb-4 md:max-w-lg">
+                Streamline your supply chain with our local purchase order
+                financing,
+              </h1>
+              <p className="text-start text-xl mb-6 md:max-w-md">
+                Are you a small or medium-sized business looking to expand your
+                operations and take on large orders? with our purchase order
+                financing you can do the following:
+              </p>
+              <Button content="Get Funding Now" classes="primary-btn btn-sm " />
             </div>
-            <div className=""></div>
-            <div className=""></div>
-          </section>
-       
+            <div className="flex items-center justify-center text-white">
+              <img
+                src={`${BaseDirectories.IMAGES_DIR}/landing/Finance/img5.png`}
+                alt="Hero"
+                className="max-w-full md:mt-16"
+              />
+            </div>
+          </div>
+          <div
+            className="grid md:grid-cols-2 grid-cols-1 justify-center  text-start items-center gap-8 mt-6"
+            data-aos="fade-up"
+          >
+            <div className="flex items-center justify-center text-white">
+              <img
+                src={`${BaseDirectories.IMAGES_DIR}/landing/Finance/img3.png`}
+                alt="Hero"
+                className="max-w-full md:mt-16"
+              />
+            </div>{" "}
+            <div className="">
+              <h1 className="font-bold text-4xl mb-4 md:max-w-lg">
+                Unlock the power of working capital.
+              </h1>
+              <p className="text-start text-xl mb-6 md:max-w-md">
+                Are you struggling to keep up with day-to-day expenses or
+                looking to expand your business? Our working capital solutions
+                provide the funds you need to do the following
+              </p>
+              <Button content="Get Funding Now" classes="primary-btn btn-sm " />
+            </div>
+          </div>
+          <div
+            className="grid md:grid-cols-2 grid-cols-1 justify-center  text-start items-center gap-8 my-6"
+            data-aos="fade-down"
+          >
+            <div className="">
+              <h1 className="font-bold text-4xl mb-4 md:max-w-lg">
+                Improve your cash flow with invoice discounting finance.
+              </h1>
+              <p className="text-start text-xl mb-6 md:max-w-md">
+                Are you tired of waiting for your customers to pay their
+                invoices? We provide the funding you need to improve your cash
+                flow and keep your business running smoothly. How?
+              </p>
+              <Button content="Get Funding Now" classes="primary-btn btn-sm " />
+            </div>
+            <div className="flex items-center justify-center text-white">
+              <img
+                src={`${BaseDirectories.IMAGES_DIR}/landing/Finance/img2.png`}
+                alt="Hero"
+                className="max-w-full md:mt-16"
+              />
+            </div>
+          </div>
+        </section>
       </section>
     </LandingLayout>
   );
--- src/pages/landing/SavingsInvestment.tsx
+++ src/pages/landing/SavingsInvestment.tsx
@@ -10,7 +10,7 @@ export default function SavingsInvestment() {
         name="description"
         content="Explore savings and investment opportunities with Loadstone Financial"
       />
-      <section className="h-screen text-3xl flex justify-center items-center">
+      <section className="">
         <div
           className=""
           style={{
@@ -19,34 +19,34 @@ export default function SavingsInvestment() {
             backgroundPosition: "center",
           }}
         >
-          <div className="text-white text-start flex flex-col mt-20 gap-6 ">
+          <div className="w-full text-white text-start flex flex-col pt-20 gap-6 md:mx-16 pb-4">
             <h3 className="uppercase text-start text-brand-purple-light">
               {" "}
-              finance
+              SAVING & INVESTMENT
             </h3>
-            <h1 className="text-5xl font-bold mb-4 md:max-w-lg">
-              Access instant loan for your business
+            <h1 className="text-5xl font-bold mb-4 max-w-4xl">
+              Achieve your business dream with a saving plan
             </h1>
             <p className="text-xl md:max-w-md">
               No long wait, no need for collateral, get up to N10M loan within
               six hours
             </p>
             <div className="mt-8">
-              <Button content="Get Loan" classes="secondary-btn btn-sm " />
+              <Button content="Start Saving" classes="secondary-btn btn-sm " />
             </div>
           </div>
           <div />
         </div>
         <div className="">
-          <div className="bg-white rounded-3xl">
+          <div className="bg-white max-w-2xl rounded-3xl">
             <img
-                src={`${BaseDirectories.IMAGES_DIR}/landing/Finance/market.png`}
-                alt="Hero"
-                className="max-w-full md:mt-16"
-              />
-              <h2 className="">
-                Explore savings and investment opportunities with Loadstone
-              </h2>
+              src={`${BaseDirectories.IMAGES_DIR}/landing/Finance/img4.png`}
+              alt="Hero"
+              className="max-w-full md:mt-16"
+            />
+            <h2 className="">
+              Explore savings and investment opportunities with Loadstone
+            </h2>
           </div>
           <div className=""></div>
         </div>
