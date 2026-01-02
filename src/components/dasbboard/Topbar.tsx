d8f210da775f6c92b14eb7b6595963349c525efc
Mheyrie <omota4fidelity@gmail.com>
2026-01-02 18:22:51 +0000

    updating

--- src/components/dasbboard/Topbar.tsx
+++ src/components/dasbboard/Topbar.tsx
@@ -1,7 +1,17 @@
-export default function Topbar() {
+export default function Topbar({ isOpen,
+  onClick }: {   isOpen: boolean;
+  onClick: () => void;}) {
   return (
-    <header className="bg-yellow-500 h-20 min-w-full">
-        
+    <header className="flex items-center px-4 border-b lg:hidden h-14 min-w-full">
+      <button className="" onClick={onClick}>
+        <div className="flex items-center justify-center">
+          <div className="relative top-0 right-4  w-8 h-6">
+            <span className={`absolute h-0.5 w-6 bg-gray-600 transform transition duration-300 ease-in-out top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${isOpen ? "rotate-55" : ""}`}/>
+            <span className={`absolute h-0.5 w-6 bg-gray-600 transform transition duration-300 ease-in-out top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${isOpen ? "-rotate-45" : "translate-y-2"}`}/>
+            <span className={`absolute h-0.5 w-6 bg-gray-600 transform transition duration-300 ease-in-out top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${isOpen ? "opacity-0" : "-translate-y-2"}`}/>
+          </div>
+        </div>
+      </button>
     </header>
-  )
+  );
 }
