347b42c460e0ef562688372caf28fec57d9299a8
Mheyrie <omota4fidelity@gmail.com>
2026-01-07 23:39:05 +0000

    Enhance StatCard component; add visibility toggle for value display and support for additional titles and icons.

--- src/pages/dashboard/Loans.tsx
+++ src/pages/dashboard/Loans.tsx
@@ -1,5 +1,9 @@
+import LoanMain from "@/components/dashboard/loan/LoanMain";
+
 export default function Loans() {
   return (
-    <div>Loans</div>
-  )
+    <div>
+      <LoanMain />
+    </div>
+  );
 }
