8ecab384d5bec1106a10b16ed75ac5883f5a4734
Mheyrie <omota4fidelity@gmail.com>
2026-01-24 20:29:36 +0000

    feat: add navigation links to service cards in dashboard overview

--- src/components/dashboard/overview/Services.tsx
+++ src/components/dashboard/overview/Services.tsx
@@ -1,22 +1,27 @@
 import { mdiCashMultiple, mdiFlowerTulip, mdiHandCoin, mdiHome } from "@mdi/js";
 import Icon from "@mdi/react";
+import { Link } from "react-router-dom";
 
 const ServicesCards = [
   {
     title: "Loan",
     Icon: mdiHandCoin,
+    link: "/dashboard/loans",
   },
   {
     title: "Invest",
     Icon: mdiFlowerTulip,
+    link: "/dashboard/invest",
   },
   {
     title: "Save",
     Icon: mdiHome,
+    link: "/dashboard/wallet",
   },
   {
     title: "Pay Bills",
     Icon: mdiCashMultiple,
+    link: "/dashboard/wallet",
   },
 ];
 
@@ -24,11 +29,14 @@ export default function Services() {
   return (
     <div className=" grid md:grid-cols-4 grid-cols-2 gap-6 my-4 rounded-2xl">
       {ServicesCards.map((service, index) => (
-        <span className="bg-white flex items-center gap-2 order-2 p-2 py-4 shadow-2xl rounded-lg hover:bg-gray-100 cursor-pointer justify-center" key={index}>
+        <Link to={service.link}> <span className="bg-white flex items-center gap-2 order-2 p-2 py-4 shadow-2xl rounded-lg hover:bg-gray-100 cursor-pointer justify-center" key={index}>
           <Icon path={service.Icon} size={1} className="text-brand-purple" />
           <h4 className="font-bold">{service.title}</h4>
         </span>
-      ))}
-    </div>
+        </Link>
+
+      ))
+      }
+    </div >
   );
 }
